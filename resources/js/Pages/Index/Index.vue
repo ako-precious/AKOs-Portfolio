c4f4c8fb87a343fb410b6c53366ceda1cd6a1110
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-12 14:03:29 -0400

    v

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -13,12 +13,12 @@
 </template>
 
 <script setup>
-import MainLayout from "../../Layouts/BannerLayout.vue";
-import Fullstack from "../../Layouts/Fullstack.vue";
 import AboutLayout from "../../Layouts/AboutLayout.vue";
-import Works from "../../Layouts/Works.vue";
 import Contact from "../../Layouts/Contact.vue";
-import ToolStack from "../../Layouts/ToolStack.vue";
+import Fullstack from "../../Layouts/Fullstack.vue";
+import MainLayout from "../../Layouts/BannerLayout.vue";
 import Message from "../../Layouts/contact/Message.vue";
+import ToolStack from "../../Layouts/ToolStack.vue";
 import SocialMedia from "../../Layouts/contact/SocialMedia.vue";
+import Works from "../../Layouts/Works.vue";
 </script>
