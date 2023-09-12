a47e98ec6bd572f86225853b005f99f85a5d1197
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-12 09:58:23 -0400

    vercel adjustment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <title>Ako's Portfolio</title>
+
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logo.jpg') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('logo.jpg') }}">
+        <link rel="mask-icon" href="{{ asset('logo.png') }}">
+         <link rel="shortcut icon" href="{{ asset('logo.png') }}">
         <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
