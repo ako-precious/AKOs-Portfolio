9145d0fd54c8ee6b7bf89075e21256858c68d8fc
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-18 12:01:24 -0400

    adding routes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,7 +21,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/TextPlugin.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/ScrollTrigger.min.js"></script>
 
-    
+    @routes()
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/scrollbar.js'])
     @inertiaHead()
     <style>
