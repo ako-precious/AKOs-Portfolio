3c2fa68e3fb57d472598a3a4c3b270dbdb1ea900
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-18 11:47:02 -0400

    e go work

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     resolve: {
-        alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
         dedupe: ["vue"],
     },
     plugins: [
