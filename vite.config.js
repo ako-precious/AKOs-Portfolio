5ff5e0c46590712deffeb826b18349566159a8b0
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-18 09:34:31 -0400

    vit

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,20 @@ export default defineConfig({
         }),
         vue({
             template:{
-                base: null,
-                includeAbsolute:false
+                transformAssetUrls:{
+
+                    base: null,
+                    includeAbsolute:false
+                }
             }
         })
     ], 
+    build: {
+        /** If you set esmExternals to true, this plugins assumes that 
+              all external dependencies are ES modules */
+    
+        commonjsOptions: {
+          esmExternals: true,
+        },
+      },
 });
