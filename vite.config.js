4b8d99362f3f3b88a5fbb5b58bde12233c70f1eb
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-21 19:13:41 -0400

    i dont know how to make this work

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,59 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        vue(),
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-            'resources/js/scrollbar.js',
-        ]),
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/scrollbar.js"
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                   
+                    base: null,
+
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    // build: {
+    //     /** If you set esmExternals to true, this plugins assumes that 
+    //       all external dependencies are ES modules */
+
+    //     commonjsOptions: {
+    //         esmExternals: true,
+    //     },
+    //     rollupOptions: {
+    //         external: ["vue"],
+    //         resolve: {
+    //             dedupe: ["vue"],
+    //         },
+    //         output: {
+    //             globals: {
+    //                 vue: "Vue",
+    //             },
+    //         },
+    //     },
+    // },
+    // rollupOptions: {
+    //     external: ["vue"],
+    //     resolve: {
+    //         dedupe: ["vue"],
+    //     },
+    //     output: {
+    //         globals: {
+    //             vue: "Vue",
+    //         },
+    //     },
+    // },
+
+    // build: {
+    // rollupOptions: {
+    //     external: ['vue'],
+    //   },
+    // },
 });
