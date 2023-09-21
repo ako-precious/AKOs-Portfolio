b462b6a11c1b2f88e603a87658b683e31d69df6d
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-21 18:42:36 -0400

    keep trying

--- vite.config.js
+++ vite.config.js
@@ -4,67 +4,10 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/js/scrollbar.js",
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
-                    // base: null,
-
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        vue(),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
-    // build: {
-    //     /** If you set esmExternals to true, this plugins assumes that 
-    //       all external dependencies are ES modules */
-
-    //     commonjsOptions: {
-    //         esmExternals: true,
-    //     },
-    //     rollupOptions: {
-    //         external: ["vue"],
-    //         resolve: {
-    //             dedupe: ["vue"],
-    //         },
-    //         output: {
-    //             globals: {
-    //                 vue: "Vue",
-    //             },
-    //         },
-    //     },
-    // },
-    // rollupOptions: {
-    //     external: ["vue"],
-    //     resolve: {
-    //         dedupe: ["vue"],
-    //     },
-    //     output: {
-    //         globals: {
-    //             vue: "Vue",
-    //         },
-    //     },
-    // },
-
-    // build: {
-    // rollupOptions: {
-    //     external: ['vue'],
-    //   },
-    // },
 });
