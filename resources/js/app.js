357190a4710395712e3d975c6531f74170363aca
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-21 19:17:46 -0400

    ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,16 +4,23 @@ import { createInertiaApp } from '@inertiajs/vue3'
 // import { createInertiaApp, Link } from "@inertiajs/vue3";
 // import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
  
-createInertiaApp({
-  resolve: async (name) => {
-    const pages = import.meta.glob('./Pages/**/*.vue')
+// createInertiaApp({
+//   resolve: async (name) => {
+//     const pages = import.meta.glob('./Pages/**/*.vue')
 
-    return (await pages[`./Pages/${name}.vue`]())
-  },
+//     return (await pages[`./Pages/${name}.vue`]())
+//   },
+//   setup({ el, App, props, plugin }) {
+//     createApp({ render: () => h(App, props) })
+//       .use(plugin)
+//       .mount(el)
+//   },
+//  })
+createInertiaApp({
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
+    return createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
- })
-
+})
