3bd9c7a002aa5361be580d632249a9c0bc6a3456
AKO PRECIOUS <akoprecious58@gmail.com>
2023-09-25 09:22:22 -0400

    b

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import  inertiaVue from './node_modules/vue/dist/vue.esm-bundler.js';
+// import  inertiaVue from './node_modules/vue/dist/vue.esm-bundler.js';
 
 export default defineConfig({
     plugins: [
@@ -22,13 +22,7 @@ export default defineConfig({
                 },
             },
         }),
-        // inertiaVue({
-        //     resolve: async (name) => {
-        //       const pages = import.meta.glob('./Pages/**/*.vue')
         
-        //       return (await pages[`./Pages/${name}.vue`]())
-        //     },
-        //   }),
     ],
     resolve: {
         dedupe: ["vue"],
